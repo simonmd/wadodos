12ba40bc2fd4d9add4998fb19263cdf73cfc5783
Simon Rascovsky <simonmd@gmail.com>
2011-08-07 12:11:00 -0500

    Quite las lines de feedback de el login y pass, torque ya se sue funcionan.

--- mechanize_login.rb
+++ mechanize_login.rb
@@ -6,6 +6,4 @@ page = agent.get("http://localhost:8080/wado2/study.xml?ModalitiesInStudy=CT")
 login_form = page.forms.first
 login_form.j_username = 'admin'
 login_form.j_password = 'admin'
-
-puts login_form.j_username , login_form.j_password
 page = agent.submit(login_form, login_form.buttons.first)
